2d330586cb1079437928c5989e2d5c49aca62b7a
Hai Hw <nguyenhuuhiephai@gmail.com>
2014-04-30 11:10:18 +0800

    Initial Commit

---
+++ IDDCaller/HWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HWAppDelegate.h
+//  IDDCaller
+//
+//  Created by Hai Hw on 30/4/14.
+//  Copyright (c) 2014 Hai Hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IDDCaller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDDCaller
+//
+//  Created by Hai Hw on 30/4/14.
+//  Copyright (c) 2014 Hai Hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+    }
+}
